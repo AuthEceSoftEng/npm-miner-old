20c13d53dc1a8117110ad7fd975ed3b87c504de1
Michael Tsapanos <mich.tsap@gmail.com>
2017-10-12 13:56:13 +0300

    Shoot material. Use bootstrap 4 components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import Grid from 'material-ui/Grid';
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        align="center"
-      >
-        <Grid item>
-          <Header />
-        </Grid>
-        <Grid item>
-
-        </Grid>
-        <Grid item>
-
-        </Grid>
-        <Grid item>
-          <h1>Test</h1>
-        </Grid>
-        <Grid item>
-          <h1>Test</h1>
-        </Grid>
-      </Grid>
+      <div>
+        <Header></Header>
+        <h1> Test </h1>
+      </div>
     );
   }
 }
--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -1,40 +1,49 @@
 import React, { Component } from 'react';
-import Typography from 'material-ui/Typography';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Button from 'material-ui/Button';
-import Grid from 'material-ui/Grid';
-import logo from './logo.svg';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+// import logo from './logo.svg';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-      <AppBar position="fixed">
-        <Toolbar>
-          <Grid
-            container
-            direction="row"
-            align="baseline"
-            justify="space-between"
-            wrap = "no-wrap"
-            spacing = {24}
-          >
-            <Grid item xs={2} >
-              <img src={logo} className="App-logo" alt="logo" height="42" width="42"/>
-            </Grid>
-            <Grid item xs={4}>
-              <Typography type="title" color="inherit">
-                Npm-Miner
-              </Typography>
-            </Grid>
-            <Grid item xs={3} hidden >
-            </Grid>
-            <Grid item xs={3}>
-              <Button color="contrast">Login</Button>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
+      <div>
+        <Navbar color="primary" dark expand="md">
+          <NavbarBrand href="/">Npm-miner</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink>Menu</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink >
+                  Logout
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
