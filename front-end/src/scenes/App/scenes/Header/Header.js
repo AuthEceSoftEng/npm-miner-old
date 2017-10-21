6c5ed9399a24c3d6a90e0b02f19e3a5e427a55b8
Michael Tsapanos <mich.tsap@gmail.com>
2017-10-21 22:26:59 +0300

    Fixed a is descendant of a warning

--- front-end/src/scenes/App/scenes/Header/Header.js
+++ front-end/src/scenes/App/scenes/Header/Header.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-// import { Button } from 'react-bootstrap';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import styled from 'styled-components';
 import logo from './logo.svg';
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const HeaderWrapper = styled.div`
   .navbar,
@@ -18,13 +18,12 @@ const HeaderWrapper = styled.div`
   .navbar-brand > img {
     padding: 7px 14px;
   }
-  .navbar-brand > a {
+
+  .navbar-default .navbar-brand:hover,
+  .navbar-default .navbar-brand:focus,
+  a.navbar-brand {
     color: ${props => props.theme.colors.typography};
     font-weight: 900;
-  }
-
-  .navbar-default .navbar-toggle .icon-bar {
-    background-color: ${props => props.theme.colors.typography};
   }
 
   .navbar-default .navbar-toggle:hover,
@@ -33,8 +32,11 @@ const HeaderWrapper = styled.div`
     box-shadow: 2px 2px 5px grey;
   }
 
-  .navbar-default .navbar-nav > li > a > a {
+  .navbar-default .navbar-nav > .active > a,
+  .navbar-default .navbar-nav > .active > a:focus,
+  .navbar-default .navbar-nav > li > a {
     color: ${props => props.theme.colors.typography};
+    background-color: ${props => props.theme.colors.primary};
   }
 `;
 
@@ -47,19 +49,19 @@ class Header extends Component {
             <Navbar.Brand>
               <Link to="/">
                 <img src={logo} alt="" width="72" height="72" />
-                <a>Npm-Miner</a>
+                Npm-Miner
               </Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <NavItem>
-                <Link to="/playground">PlayGround</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/about">About</Link>
-              </NavItem>
+              <LinkContainer to="/playground">
+                <NavItem eventKey={1}>PlayGround</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <NavItem eventKey={2}>About</NavItem>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
