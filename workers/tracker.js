79961445e61ac5238883aa5ba5f44e6c9100e930
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2017-11-29 10:05:45 +0200

    Optimization of the tracker

--- workers/tracker.js
+++ workers/tracker.js
@@ -7,8 +7,6 @@ const consts = require('./consts');
 const logger = bunyan.createLogger({ name: 'tracker' });
 const npmdb = require('nano')('http://couchdb.npm-miner.com:5986/npm-registry');
 Promise.promisifyAll(npmdb);
-const meta = require('nano')('http://couchdb.npm-miner.com:5986/npm-meta');
-Promise.promisifyAll(meta);
 const url =
   'amqp://localhost' ||
   process.env.CLOUDAMQP_URL ||
@@ -16,15 +14,14 @@ const url =
 
 let myConn;
 let channel;
-const limit = 1400;
+const limit = 2000;
 const last_sequence_id = 'last_sequence';
 const q = 'filter';
+const _ = require('lodash');
 
 new CronJob(
-  '00 32 * * * *',
+  '00 00 * * * *',
   function() {
-    let last_seq_doc;
-    let last_seq;
     // Get the latest change
     logger.info('It is time to track the changes');
     // Connet to rabbit
@@ -36,59 +33,35 @@ new CronJob(
       })
       .then(ch => {
         channel = ch;
-        // Get the last sequence
-        return meta.getAsync(last_sequence_id);
+        // Get the number of documents in the db
+        logger.info(`Retrieving documents`);
+        return npmdb.infoAsync();
       })
       .then(body => {
-        last_seq_doc = body;
-        logger.info(`Last sequence checked: ${last_seq_doc.last_sequence}`);
+        let total = body.doc_count;
+        let totalPages = Math.ceil(total / limit);
+        logger.info(`Total documents: ${total}`);
+        logger.info(`Total pages: ${totalPages}`);
+        var page = _.random(1, totalPages);
+        logger.info(`Page: ${page}`);
         // Fetch the changes since the last sequence
-        return npmdb.changesAsync({ limit, since: last_seq_doc.last_sequence });
-      })
-      .then(body => {
-        last_seq = body.last_seq;
-        return Promise.map(body.results, change => {
-          if (change.id.charAt(0) === '_') {
-            logger.info('skipping');
-            return Promise.resolve('skipping');
-          } else if (change.deleted) {
-            logger.info(`deleting ${change.id}`);
-            const job = {
-              task: consts.DELETE_PACKAGE,
-              package_name: change.id
-            };
-            return Promise.all([
-              channel.assertQueue(q),
-              channel.sendToQueue(q, Buffer.from(JSON.stringify(job)))
-            ]);
-          } else if (change.changes[0].rev.substring(0, 2) === '1-') {
-            logger.info(`creating ${change.id}`);
-            const job = {
-              task: consts.CREATE_PACKAGE,
-              package_name: change.id
-            };
-            return Promise.all([
-              channel.assertQueue(q),
-              channel.sendToQueue(q, Buffer.from(JSON.stringify(job)))
-            ]);
-          } else {
-            const job = {
-              task: consts.UPDATE_PACKAGE,
-              package_name: change.id
-            };
-            logger.info(`updating ${change.id}`);
-            return Promise.all([
-              channel.assertQueue(q),
-              channel.sendToQueue(q, Buffer.from(JSON.stringify(job)))
-            ]);
-          }
+        return npmdb.listAsync({
+          include_docs: true,
+          limit: limit,
+          skip: (page - 1) * limit
         });
       })
-      .then(() => {
-        logger.info('done');
-        // writing last sequence
-        last_seq_doc.last_sequence = last_seq;
-        return meta.insertAsync(last_seq_doc);
+      .then(body => {
+        return Promise.map(body.rows, doc => {
+          logger.info(doc.doc.name);
+          const job = {
+            package_name: doc.doc.name
+          };
+          return Promise.all([
+            channel.assertQueue(q),
+            channel.sendToQueue(q, Buffer.from(JSON.stringify(job)))
+          ]);
+        });
       })
       .catch(err => {
         logger.error(err);
