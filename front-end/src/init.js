e28f198d6eddf0ec2fadd9417ef717f5f0662531
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2018-05-11 11:50:46 +0300

    Debugging env vars

--- front-end/src/init.js
+++ front-end/src/init.js
@@ -1,6 +1,7 @@
 import GoogleAnalytics from 'react-ga';
 
 export default function initGA() {
+    console.log(process.env.NODE_ENV)
     if (process.env.NODE_ENV === 'production') {
         GoogleAnalytics.initialize('UA-17339437-5')
     }
